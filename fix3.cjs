ed969e8c6f3baa4716b2891d8bf3dd35b801ca93
macgongming-beep <mac.gongming@gmail.com>
2026-05-25 17:12:40 +0900

    chore: cleanup scratch files

--- fix3.cjs
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let desktopCode = fs.readFileSync('src/components/DesktopCalendar.tsx', 'utf-8');
-
-desktopCode = desktopCode.replace('language={language ?? \\\'ko\\\'}', "language={'ko'}");
-desktopCode = desktopCode.replace(', PLACE_PRESETS_MAX', '');
-
-fs.writeFileSync('src/components/DesktopCalendar.tsx', desktopCode);
--- patch_desktop_imports2.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/components/DesktopCalendar.tsx', 'utf-8');
-
-code = code.replace(
-  "import { CommentSection, type MentionUser } from './CommentSection'",
-  "import { CommentSection, type MentionUser } from './CommentSection'\nimport { loadPlacePresets, savePlacePresets, normalizePlacePresets, PLACE_PRESETS_MAX } from '../lib/placePresets'\nimport type { PlacePreset } from '../lib/placePresets'\nimport { PlacePresetEditor } from './admin/AdminMobileCalendar'"
-);
-
-fs.writeFileSync('src/components/DesktopCalendar.tsx', code);
